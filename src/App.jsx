ef1dd08ae34332beae47fa1e46ed817c9940f356
andreyviana <andreyviana125@gmail.com>
2023-06-09 15:07:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+
+
+function App() {
+
+	const [search, setSearch] = useState("");
+	const [user, setUser] = useState([]);
+
+	const handleSearch = () => {
+		axios
+			.get(`https://api.github.com/users/${search}`)
+			.then((res) => setUser(res.data))
+			.catch((err) => {
+				console.error("ops! ocorreu um erro" + err)
+			});
+	}
+
+	return (
+		<main>
+			<div className="form">
+				<h1>Buscar perfil do github</h1>
+				<input 
+					type="text" 
+					placeholder="Nome de usuário"
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<button onClick={handleSearch}>Buscar</button>
+			</div>
+			<div className="content">
+				<img src={user.avatar_url ? user.avatar_url : "https://avatars.githubusercontent.com/u/64756172?v=4"} alt="" />
+				<p>{user.name ? user.name : "(Nome do usuário)"}</p>
+				<p>{user.bio ? user.bio : "(Descrição)"}</p>
+			</div>
+		</main>
+	);
+}
+
+export default App;
